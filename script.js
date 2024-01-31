0e84a5ff69579fc77ea0dc4939e9037a0ccb652c
rahma-mohamed <layanayman38@gmail.com>
2024-01-31 21:45:38 +0200

    upload project

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function downloadPDF() {
     // Replace 'your_pdf_file.pdf' with the actual file path or URL
-    const pdfFilePath = './rahma_mohamed.pdf';
+    const pdfFilePath = './rahma_mohamed_hammad.pdf';
 
     // Creating an invisible link element
     const downloadLink = document.createElement('a');
     downloadLink.href = pdfFilePath;
-    downloadLink.download = './rahma_mohamed.pdf';
+    downloadLink.download = './rahma_mohamed_hammad.pdf';
 
     // Appending the link to the body
     document.body.appendChild(downloadLink);
